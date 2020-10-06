45902177087abaec694ade57ea40f6798e2c800a
Eleftherios Moschos <tmoschos@bigpond.com>
2020-10-06 22:53:11 +1030

    Project set up

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import { StoreProvider } from "./utils/GlobalState";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <StoreProvider>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            <Route component={NoMatch} />
+          </Switch>
+        </StoreProvider>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
