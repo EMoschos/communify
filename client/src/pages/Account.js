ec27edf1b28c3109f0d6852b921dfc2fd216554c
Eleftherios Moschos <tmoschos@bigpond.com>
2020-10-21 14:57:41 +1030

    trying to get modal to work on heroku

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -140,17 +140,16 @@ const Account = () => {
                 </div>
             </div>
             <script>
-            {document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(elems);
-    var elems1 = document.querySelectorAll('.modal');
-    M.Modal.init(elems1);
-    var elems2 = document.querySelectorAll('.tabs');
-    M.Tabs.init(elems2);
-})}
+                {`${document.addEventListener('DOMContentLoaded', function () {
+                    var elems = document.querySelectorAll('.collapsible');
+                    M.Collapsible.init(elems);
+                    var elems1 = document.querySelectorAll('.modal');
+                    M.Modal.init(elems1);
+                    var elems2 = document.querySelectorAll('.tabs');
+                    M.Tabs.init(elems2);
+                })}`}
             </script>
         </div>
-        
     );
 };
 
