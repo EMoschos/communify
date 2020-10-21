038ea9b26067ffc02b30ebfba58393d03800656b
Eleftherios Moschos <tmoschos@bigpond.com>
2020-10-21 14:01:19 +1030

    updated html page head for materialise

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -6,7 +6,6 @@ import M from 'materialize-css'
 import UserContext from "../utils/UserContext"
 import TaskContext from "../utils/TaskContext"
 import UpdateUser from "../components/UpdateUser"
-import "./materializeJS"
 
 const Account = () => {
     const user = useContext(UserContext)
@@ -58,6 +57,14 @@ const Account = () => {
     // const [editUser, setEditUser] = useState(false)
 
     // const [allTasks, setAllTask] = useState([])
+    document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('.collapsible');
+        M.Collapsible.init(elems);
+        var elems1 = document.querySelectorAll('.modal');
+        M.Modal.init(elems1);
+        var elems2 = document.querySelectorAll('.tabs');
+        M.Tabs.init(elems2);
+    });
 
     return (
         <div className="container">
