fbb6cdcb38265ad49228610a57d9bb44e026811d
Eleftherios Moschos <tmoschos@bigpond.com>
2020-10-21 12:02:20 +1030

    updated mongoDB database name

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Connect to the Mongo DB
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms",
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/communifydb",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
