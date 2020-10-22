9a1551899f7075480b673e7d71f9c82f4ae447fb
Eleftherios Moschos <tmoschos@bigpond.com>
2020-10-22 19:03:25 +1030

    test

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
 }
 
 // Connect to the Mongo DB
