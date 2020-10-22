695cddafd33f7b00e649e001398721cf3260ef93
Eleftherios Moschos <tmoschos@bigpond.com>
2020-10-22 18:35:52 +1030

    Updated server file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, 'client/build')));
-  
+  app.use(routes)
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
   });
